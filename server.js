4e08ae0b6b3c676e0744e56dbc966f4ce47efa37
José Manuel Sánchez Arredondo <106613839+JMSASanchezUwU@users.noreply.github.com>
2025-05-07 23:08:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 // Conexión a MongoDB
